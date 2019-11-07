dcbc53039382580815a3acb6032340ccf8415297
774767 <monidip.bose@cognizant.com>
2019-11-07 19:38:13 +0530

    Some changes

---
+++ server/Persistence/VegaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Persistence
+{
+    public class VegaDbContext:DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using server.Persistence;
 
 namespace server
 {
@@ -24,6 +26,7 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options=>options.UseSqlServer(Configuration["ConnectionString:Default"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
