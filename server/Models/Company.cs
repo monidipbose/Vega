4639d3a161b7871d4a04f874bbbe0141463fd448
774767 <monidip.bose@cognizant.com>
2019-11-07 16:02:43 +0530

    Add project files.

---
+++ server/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+
+        public Company()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ server/Models/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Company Company { get; set; }
+        public int CompantId { get; set; }
+    }
+}
